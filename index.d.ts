1b3e876f1a9033387e85c7c3629123f1ccdbdef2
kevin.krug <kevin.krug@aperto.com>
2019-06-11 16:40:53 +0200

    :ok_hand: code review requested changes. adds interfaces to namespace. updates return type. #138

--- index.d.ts
+++ index.d.ts
@@ -1,47 +1,50 @@
 import { RequestHandler, Request, Response, NextFunction } from "express";
 
-declare namespace RateLimit {}
+declare namespace RateLimit {
+  interface Options {
+    max?: number;
+    message?: any;
+    headers?: boolean;
+    windowMs?: number;
+    store?: Store | any;
+    statusCode?: number;
+    skipFailedRequests?: boolean;
+    skipSuccessfulRequests?: boolean;
 
-interface Options {
-  max?: number;
-  message?: any;
-  headers?: boolean;
-  windowMs?: number;
-  store?: Store | any;
-  statusCode?: number;
-  skipFailedRequests?: boolean;
-  skipSuccessfulRequests?: boolean;
+    skip?(req?: Request, res?: Response): boolean;
 
-  skip?(req?: Request, res?: Response): boolean;
+    onLimitReached?(req?: Request, res?: Response): void;
 
-  onLimitReached?(req?: Request, res?: Response): void;
+    handler?(req: Request, res: Response, next?: NextFunction): void;
 
-  handler?(req: Request, res: Response, next?: NextFunction): void;
+    keyGenerator?(req: Request, res?: Response): string | Request["ip"];
+  }
 
-  keyGenerator?(req: Request, res?: Response): string | Request["ip"];
+  interface Store {
+    hits: {
+      [key: string]: number;
+    };
+
+    resetAll(): void;
+
+    resetTime: number;
+    setInterval: NodeJS.Timeout;
+
+    resetKey(key: string | any): void;
+
+    decrement(key: string | any): void;
+
+    incr(key: string | any, cb: (err?: Error, hits?: number) => void): void;
+  }
+
+  interface RateLimitReturnType extends RequestHandler {
+    resetKey(key: string | any): void;
+    resetIp(key: string | any): void;
+  }
 }
 
-interface Store {
-  hits: {
-    [key: string]: number;
-  };
-
-  resetAll(): void;
-
-  resetTime: number;
-  setInterval: NodeJS.Timeout;
-
-  resetKey(key: string | any): void;
-
-  decrement(key: string | any): void;
-
-  incr(key: string | any, cb: (err?: Error, hits?: number) => void): void;
-}
-
-interface RateLimitReturnType {
-  (req: Request, res: Response, next: NextFunction): void;
-}
-
-declare function RateLimit(options?: Options): RateLimitReturnType;
+declare function RateLimit(
+  options?: RateLimit.Options
+): RateLimit.RateLimitReturnType;
 
 export = RateLimit;
